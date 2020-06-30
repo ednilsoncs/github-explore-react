ee8c8c87ce9b37bea3f775d7c5a0c0c6de15d749
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-29 22:41:21 -0300

    feat: added add repositories screen

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,73 @@
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import {
+  Title, Form, Repositories, Error,
+} from './styles';
+import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepository = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o nome autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const { data: repository } = await api.get<Repository>(`repos/${newRepo}`);
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  };
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore" />
+      <Title>Dashboard</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      {inputError
+        && (
+          <Error>
+            {inputError}
+          </Error>
+        )}
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>
+                {repository.description}
+              </p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
